cbe0d1d4cd2c1e74d836ec1420a063965eedbb29
Ben Hemann <benhemann@yahoo.com>
2021-01-05 15:00:28 -0600

    Created the quiz questions and answers. Used a counter to keep track of right or wrong.

--- js/app.js
+++ js/app.js
@@ -3,22 +3,56 @@
 console.log('This is the app.js, connected to the index.');
 
 //prompt our user for input data
-var userName = prompt('What is your username?');
-console.log('The users name is: ' + userName);
+// var userName = prompt('What is your username?');
+// console.log('The users name is: ' + userName);
 
-//greet the user
-alert('Hello ' + userName);
+// //greet the user
+// alert('Hello ' + userName);
 
-//Validate user input.
-var toLowerCase = userName.toLowerCase();
-console.log(toLowerCase);
+// //Validate user input.
+// var toLowerCase = userName.toLowerCase();
+// console.log(toLowerCase);
 
 //Validate the input using conditional logic using strict equals.
+var counter = 0;
+var question1 = prompt('What month was I born?').toLowerCase();
 
-
-if(toLowerCase === 'craig' || toLowerCase === 'bob'){
-    alert('Welcome back to the page, ' + userName);
-} else {
-    alert('Welcome new user to the page.');
+if (question1 ==='october' || question1 === 'oct'){
+    alert('You got that one right!');
+    counter += 1;
+} else{
+    alert('Sorry, that wasn\'t the right answer');
 }
+
+var question2 = prompt('Did I graduate Xavier high school ins 2009 or 2010?').toLowerCase();
+if (question2 ==='2009'){
+    alert('You got that one right!');
+    counter += 1;
+} else{
+    alert('Sorry, that wasn\'t the right answer');
+}
+
+var question3 = prompt('True or False, I did 2.5 years at Kirkwood Community College').toLowerCase();
+if (question3 ==='True' || question3 === 'true'){
+    alert('You got that one right!');
+    counter += 1;
+} else{
+    alert('Sorry, that wasn\'t the right answer');
+}
+var question4 = prompt('True or False, I joined the Marine Corps in 2012').toLowerCase();
+if (question4 ==='false' || question4 === 'False'){
+    alert('You got that one right!');
+    counter += 1;
+} else{
+    alert('Sorry, that wasn\'t the right answer');
+}
+var question5 = prompt('What degree did I graduate the University of Iowa with? Social Informatics or Computer Science').toLowerCase();
+if (question5 ==='social informatics'){
+    alert('You got that one right!');
+    counter += 1;
+} else{
+    alert('Sorry, that wasn\'t the right answer');
+}
+alert('You got ' + counter + ' answers right!')
+
 
