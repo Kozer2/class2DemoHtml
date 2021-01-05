e4c2fb1ad7b2b5b91d4965f9f93c4bbc2571bf1b
Ben Hemann <benhemann@yahoo.com>
2021-01-05 15:02:39 -0600

    Added comments

--- js/app.js
+++ js/app.js
@@ -1,22 +1,13 @@
 'use strict';
-//To do
+//
 console.log('This is the app.js, connected to the index.');
 
-//prompt our user for input data
-// var userName = prompt('What is your username?');
-// console.log('The users name is: ' + userName);
 
-// //greet the user
-// alert('Hello ' + userName);
-
-// //Validate user input.
-// var toLowerCase = userName.toLowerCase();
-// console.log(toLowerCase);
-
-//Validate the input using conditional logic using strict equals.
+//Create a counter to track right or wrong answers.
+//Create 5 questions that prompt the user to answer based on the info they read about me. 
+//Tell the user if they got a question right or wrong, and at the end tell them how many they got right in total.
 var counter = 0;
 var question1 = prompt('What month was I born?').toLowerCase();
-
 if (question1 ==='october' || question1 === 'oct'){
     alert('You got that one right!');
     counter += 1;
@@ -39,6 +30,7 @@ if (question3 ==='True' || question3 === 'true'){
 } else{
     alert('Sorry, that wasn\'t the right answer');
 }
+
 var question4 = prompt('True or False, I joined the Marine Corps in 2012').toLowerCase();
 if (question4 ==='false' || question4 === 'False'){
     alert('You got that one right!');
@@ -46,6 +38,7 @@ if (question4 ==='false' || question4 === 'False'){
 } else{
     alert('Sorry, that wasn\'t the right answer');
 }
+
 var question5 = prompt('What degree did I graduate the University of Iowa with? Social Informatics or Computer Science').toLowerCase();
 if (question5 ==='social informatics'){
     alert('You got that one right!');
@@ -53,6 +46,7 @@ if (question5 ==='social informatics'){
 } else{
     alert('Sorry, that wasn\'t the right answer');
 }
+
 alert('You got ' + counter + ' answers right!')
 
 
