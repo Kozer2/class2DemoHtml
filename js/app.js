7611eb3cbd276f2c60e3979c1a0011cb27cec788
Kozer2 <benhemann@yahoo.com>
2021-01-06 17:09:43 -0600

    Fixed RNG question for saying it was wrong when it was guessed correctly

--- js/app.js
+++ js/app.js
@@ -8,45 +8,45 @@ console.log('This is the app.js, connected to the index.');
 //Tell the user if they got a question right or wrong, and at the end tell them how many they got right in total.
 // alert('You are about to take a quiz with the info you learned about me. All answers are Yes or No')
 var counter = 0;
-var question1 = prompt('What month was I born?').toLowerCase();
-if (question1 ==='october' || question1 === 'oct'){
-    alert('You got that one right!');
-    counter += 1;
-} else{
-    alert('Sorry, that wasn\'t the right answer');
-}
+// var question1 = prompt('What month was I born?').toLowerCase();
+// if (question1 ==='october' || question1 === 'oct'){
+//     alert('You got that one right!');
+//     counter += 1;
+// } else{
+//     alert('Sorry, that wasn\'t the right answer');
+// }
 
-var question2 = prompt('Did I graduate Xavier high school ins 2009 or 2010?').toLowerCase();
-if (question2 ==='2009'){
-    alert('You got that one right!');
-    counter += 1;
-} else{
-    alert('Sorry, that wasn\'t the right answer');
-}
+// var question2 = prompt('Did I graduate Xavier high school ins 2009 or 2010?').toLowerCase();
+// if (question2 ==='2009'){
+//     alert('You got that one right!');
+//     counter += 1;
+// } else{
+//     alert('Sorry, that wasn\'t the right answer');
+// }
 
-var question3 = prompt('True or False, I did 2.5 years at Kirkwood Community College').toLowerCase();
-if (question3 ==='True' || question3 === 'true'){
-    alert('You got that one right!');
-    counter += 1;
-} else{
-    alert('Sorry, that wasn\'t the right answer');
-}
+// var question3 = prompt('True or False, I did 2.5 years at Kirkwood Community College').toLowerCase();
+// if (question3 ==='True' || question3 === 'true'){
+//     alert('You got that one right!');
+//     counter += 1;
+// } else{
+//     alert('Sorry, that wasn\'t the right answer');
+// }
 
-var question4 = prompt('True or False, My number 1 favorite movie is Doom.').toLowerCase();
-if (question4 ==='false' || question4 === 'False'){
-    alert('You got that one right!');
-    counter += 1;
-} else{
-    alert('Sorry, that wasn\'t the right answer');
-}
+// var question4 = prompt('True or False, My number 1 favorite movie is Doom.').toLowerCase();
+// if (question4 ==='false' || question4 === 'False'){
+//     alert('You got that one right!');
+//     counter += 1;
+// } else{
+//     alert('Sorry, that wasn\'t the right answer');
+// }
 
-var question5 = prompt('What degree did I graduate the University of Iowa with? Social Informatics or Computer Science').toLowerCase();
-if (question5 ==='social informatics'){
-    alert('You got that one right!');
-    counter += 1;
-} else{
-    alert('Sorry, that wasn\'t the right answer');
-}
+// var question5 = prompt('What degree did I graduate the University of Iowa with? Social Informatics or Computer Science').toLowerCase();
+// if (question5 ==='social informatics'){
+//     alert('You got that one right!');
+//     counter += 1;
+// } else{
+//     alert('Sorry, that wasn\'t the right answer');
+// }
 
 
 
@@ -55,9 +55,10 @@ if (question5 ==='social informatics'){
 // create a random number generator to pick a random number between 1 and 10. 
 // create counters to track number of guesses and loop iterations. 
 var number = Math.floor(Math.random() * 10) + 1;   // Found the random generator on W3 schools (https://www.w3schools.com/js/js_random.asp)
-// console.log('The random number is: ', number);
+console.log('The random number is: ', number);
 var guessCounter = 0;
 var loopCounter = 4;
+var wrong = false
 // create a while loop with if statements to check for correct answers.
 while(guessCounter < 4){
     var question6 = prompt('Guess a number between 1 and 10. You have ' + loopCounter + ' guesses remaining.');
@@ -74,41 +75,45 @@ while(guessCounter < 4){
         alert('You got lucky! Good job.');
         counter += 1;
         guessCounter = 4;
+        wrong = true
     }
     // console.log(guessCounter);
+} 
+if(wrong === false){
+    alert('You did not guess the answer. The right answer was: ' + number);
 }
-alert('You did not guess the answer. The right answer was: ' + number);
+
 
 // reset counters
-guessCounter = 0;
-loopCounter = 7;
-var userCorrect = false;
-// create an array with the right answers
-var answersFor7 =['pakistan', 'malawi', 'hong kong'];
+// guessCounter = 0;
+// loopCounter = 7;
+// var userCorrect = false;
+// // create an array with the right answers
+// var answersFor7 =['pakistan', 'malawi', 'hong kong'];
 
-while(guessCounter < 7){
-    var question7 = prompt('Name 1 country I was stationed in overseas. You have ' + loopCounter + ' guesses remaining.').toLowerCase();
-    // console.log(question7);
-    loopCounter -= 1;
-    for(var i = 0; i <= answersFor7.length; i++ ){
-        if(answersFor7[i] === question7){
-            alert('You got lucky...Again. Good job!');
-            counter += 1;
-            guessCounter = 7;
-            userCorrect = true;
-        }
-    }
-    if(guessCounter !== 7){
-        alert('That was not the right answer!');
-    }
+// while(guessCounter < 7){
+//     var question7 = prompt('Name 1 country I was stationed in overseas. You have ' + loopCounter + ' guesses remaining.').toLowerCase();
+//     // console.log(question7);
+//     loopCounter -= 1;
+//     for(var i = 0; i <= answersFor7.length; i++ ){
+//         if(answersFor7[i] === question7){
+//             alert('You got lucky...Again. Good job!');
+//             counter += 1;
+//             guessCounter = 7;
+//             userCorrect = true;
+//         }
+//     }
+//     if(guessCounter !== 7){
+//         alert('That was not the right answer!');
+//     }
     
-    guessCounter += 1;
+//     guessCounter += 1;
 
-}
-// if statement to alert the user they ran out of guesses
-if(userCorrect === false){
-    alert('You ran out of guesses!');
-}
+// }
+// // if statement to alert the user they ran out of guesses
+// if(userCorrect === false){
+//     alert('You ran out of guesses!');
+// }
 
 alert('You got ' + counter + ' answers right!')
 
